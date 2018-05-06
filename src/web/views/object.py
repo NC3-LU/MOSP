437b449b074835f1fb77e3a2a67d4db58c5754fa
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-05-06 22:24:08 +0200

    Select a schema to create a new object. Display the org owner of a schema.

--- src/web/views/object.py
+++ src/web/views/object.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, redirect, url_for, flash, \
 from flask_login import login_required, current_user
 
 from bootstrap import db, application
-from web.models import JsonObject
+from web.models import Schema, JsonObject
 
 object_bp = Blueprint('object_bp', __name__, url_prefix='/object')
 objects_bp = Blueprint('objects_bp', __name__, url_prefix='/objects')
@@ -12,12 +12,16 @@ objects_bp = Blueprint('objects_bp', __name__, url_prefix='/objects')
 @object_bp.route('/create', methods=['GET'])
 @object_bp.route('/edit/<int:object_id>', methods=['GET'])
 @login_required
-def form(object_id=None):
+def form(schema_id=None, object_id=None):
     action = "Create an object"
     head_titles = [action]
+
+    schema_id = request.args.get('schema_id', None)
+    schema = Schema.query.filter(Schema.id == schema_id).first()
 
     json_object = None
 
     return render_template('edit_object.html', action=action,
                             head_titles=head_titles,
+                            schema=schema,
                             json_object=json_object)
