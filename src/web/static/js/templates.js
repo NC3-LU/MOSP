0a5ae6faf223f0fe6421d3c4374befc560ed0cd2
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-05-07 13:26:32 +0200

    Updated index.html

--- src/web/static/js/templates.js
+++ src/web/static/js/templates.js
@@ -1,10 +1,20 @@
 
 
 var schemaHomeTemplate = _.template(
-    '<a href="<%= project_url %>" class="list-group-item list-group-item-action flex-column align-items-start">' +
+    '<a href="<%= url %>" class="list-group-item list-group-item-action flex-column align-items-start">' +
         '<div class="d-flex w-100 justify-content-between">' +
-            '<h5 class="mb-1"><%= project_name %></h5>' +
-            '<small>updated <%= project_last_update %></small>' +
+            '<h5 class="mb-1"><%= name %></h5>' +
+            '<small>updated <%= last_update %></small>' +
         '</div>' +
-        '<p class="mb-1"><%= project_description %> by <%= organization %></p>' +
+        '<p class="mb-1"><%= description %> by <%= organization %></p>' +
+    '</a>');
+
+
+var jsonObjectHomeTemplate = _.template(
+    '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">' +
+        '<div class="d-flex w-100 justify-content-between">' +
+            '<h5 class="mb-1"><%= name %></h5>' +
+            '<small>updated <%= last_update %></small>' +
+        '</div>' +
+        '<p class="mb-1"><%= description %> by <%= organization %></p>' +
     '</a>');
