196cd3100e96304fc30e5a58f0b52f53135abc41
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-05-16 21:30:12 +0200

    Now using JSONB instead of JSON. Default value is {}.

--- src/web/models/schema.py
+++ src/web/models/schema.py
@@ -1,6 +1,6 @@
 
 from datetime import datetime
-from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.dialects.postgresql import JSONB
 
 from bootstrap import db
 
@@ -11,7 +11,7 @@ class Schema(db.Model):
     name = db.Column(db.String(100))
     description = db.Column(db.String(500))
     last_updated = db.Column(db.DateTime(), default=datetime.utcnow())
-    json_schema = db.Column(JSON, default=None)
+    json_schema = db.Column(JSONB, default={})
 
     # relationship
     objects = db.relationship('JsonObject', backref='schema', lazy='dynamic',
