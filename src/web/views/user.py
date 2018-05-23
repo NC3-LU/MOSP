e92bd8bffb95c0a68f2c2d2cfd4c85de5315ecd5
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-05-23 07:51:36 +0200

    Renamed the view which lists schemas related to a user.

--- src/web/views/user.py
+++ src/web/views/user.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
+from flask_login import login_required
 
-from web.models import User
+#from web.models import User
 
 user_bp = Blueprint('user_bp', __name__, url_prefix='/user')
 
 
-@user_bp.route('/objects', methods=['GET'])
+@user_bp.route('/schemas', methods=['GET'])
 @login_required
 def me():
-    return render_template('user_objects.html')
+    return render_template('user_schemas.html')
