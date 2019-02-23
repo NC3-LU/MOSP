aacaceb9cafb72d1fae5aba51743167684b58077
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-02-24 00:28:05 +0100

    Disable actually useless HTTP methods on the Organization endpoint for the API.

--- src/web/views/api/v1/organization.py
+++ src/web/views/api/v1/organization.py
@@ -11,8 +11,5 @@ from web.views.api.v1.common import url_prefix
 blueprint_organization = manager.create_api_blueprint(
     models.Organization,
     url_prefix=url_prefix,
-    methods=['GET', 'POST', 'PUT', 'DELETE'],
-    preprocessors=dict(
-        POST=[processors.auth_func],
-        PUT=[processors.auth_func],
-        DELETE=[processors.auth_func]))
+    exclude_columns=['objects', 'schemas', 'users'],
+    methods=['GET'])
