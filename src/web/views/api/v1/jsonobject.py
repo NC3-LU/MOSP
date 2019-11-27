7d17238044671831eddc05d7863e2ffeeba151b4
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-11-27 08:46:09 +0100

    chg: improved the perforamnces on JsonObject GET many by removing useless attributes from the result.

--- src/web/views/api/v1/jsonobject.py
+++ src/web/views/api/v1/jsonobject.py
@@ -16,7 +16,24 @@ def pre_get_many(search_params=None, **kw):
     if 'order_by' not in search_params:
         search_params['order_by'] = []
     search_params['order_by'].extend(order_by)
+    search_params['exclude'] = ["description"]
 
+def get_many_postprocessor(result=None, search_params=None, **kw):
+    """Accepts two arguments, `result`, which is the dictionary
+    representation of the JSON response which will be returned to the
+    client, and `search_params`, which is a dictionary containing the
+    search parameters for the request (that produced the specified
+    `result`).
+
+    It would be better to filter these columns with a preprocessor.
+    """
+    def pop_object(elem):
+        elem.pop('json_object')
+        elem.pop('schema')
+        elem.pop('referred_to_by')
+        elem.pop('refers_to')
+        return elem
+    list(map(lambda elem: pop_object(elem), result['objects']))
 
 blueprint_object = manager.create_api_blueprint(
     models.JsonObject,
@@ -25,6 +42,7 @@ blueprint_object = manager.create_api_blueprint(
     url_prefix=url_prefix,
     methods=['GET', 'POST', 'PUT'],
     exclude_columns=['creator', 'creator_id'],
+    postprocessors=dict(GET_MANY=[get_many_postprocessor]),
     preprocessors=dict(
         GET_MANY=[pre_get_many],
         POST=[processors.auth_func, processors.check_object_edit_permission],
