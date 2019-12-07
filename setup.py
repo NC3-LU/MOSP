318767388e078ec74f481cf28eb869bcceee501b
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-12-07 14:27:49 +0100

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,25 @@
+import io
+
+from setuptools import find_packages
+from setuptools import setup
+
+with io.open("README.md", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name="MOSP",
+    version="0.8.0",
+    url="https://github.com/CASES-LU/MOSP",
+    license="AGPL",
+    maintainer="Cédric Bonhomme",
+    author_email='cedric@cedricbonhomme.org',
+    maintainer_email="",
+    description="A platform to create, edit and share security related JSON objects.",
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["flask"],
+    extras_require={"test": ["pytest", "coverage"]},
+)
