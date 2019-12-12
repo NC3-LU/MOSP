790e5ecc5b24fa0aa5a05bef82299a5d612dd0bc
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-12-12 10:13:51 +0100

    cleaning tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,66 +1,7 @@
 
-import pytest
-from flask import Flask
 from werkzeug.security import generate_password_hash
 
-from mosp.bootstrap import db as _db
-
 from mosp.models import User
-
-
-@pytest.fixture(scope='session')
-def app(request):
-    """Session-wide test `Flask` application."""
-    settings_override = {
-        'TESTING': True,
-        'SQLALCHEMY_DATABASE_URI': 'postgres://mosp:password@localhost:5432/mosp'
-    }
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mosp:password@localhost:5432/mosp'
-
-    # Establish an application context before running the tests.
-    ctx = app.app_context()
-    ctx.push()
-
-    def teardown():
-        ctx.pop()
-
-    request.addfinalizer(teardown)
-    return app
-
-
-@pytest.fixture(scope='session')
-def db(app, request):
-    """Session-wide test database."""
-    def teardown():
-        _db.drop_all()
-
-    _db.app = app
-    _db.init_app(_db.app)
-    _db.create_all()
-
-    request.addfinalizer(teardown)
-    return _db
-
-
-@pytest.fixture(scope='function')
-def session(db, request):
-    """Creates a new database session for a test."""
-    connection = db.engine.connect()
-    transaction = connection.begin()
-
-    options = dict(bind=connection, binds={})
-    session = db.create_scoped_session(options=options)
-
-    db.session = session
-
-    def teardown():
-        transaction.rollback()
-        connection.close()
-        session.remove()
-
-    request.addfinalizer(teardown)
-    return session
 
 
 def test_user(session):
@@ -70,3 +11,7 @@ def test_user(session):
     session.commit()
 
     assert user.is_admin == False
+    assert user.is_api == False
+    assert user.is_api == False
+    assert user.public_profile == True
+    assert user.check_password('password') == True
