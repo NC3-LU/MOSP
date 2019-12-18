4d108d1452fd1f926e2bf892334924a8081e6140
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-12-18 08:47:19 +0100

    Specify the address of the current Git repository for the project.

--- mosp/__init__.py
+++ mosp/__init__.py
@@ -1,9 +1,15 @@
 import os
 import subprocess
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 __version__ = (
     os.environ.get("PKGVER")
-    or subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    or subprocess.run(["git",
+                       "-C",
+                       BASE_DIR,
+                       "describe",
+                       "--tags"], stdout=subprocess.PIPE)
     .stdout.decode()
     .strip()
 )
