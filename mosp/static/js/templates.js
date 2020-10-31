944e13575d22bed384dcd85d2829bf64770ad93d
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-10-31 01:14:47 +0100

    improved WCAG 2.0 compliancy (analysis with the axe extension).

--- mosp/static/js/templates.js
+++ mosp/static/js/templates.js
@@ -1,7 +1,7 @@
 
 
 var schemaHomeTemplate = _.template(
-    '<a href="<%= url %>" class="list-group-item list-group-item-action flex-column align-items-start">' +
+    '<a href="<%= url %>" aria-label="Read more about <%= name %>" class="list-group-item list-group-item-action flex-column align-items-start">' +
         '<div class="d-flex w-100 justify-content-between">' +
             '<h5 class="mb-1"><%= name %></h5>' +
             '<small>updated <%= last_update %></small>' +
@@ -11,7 +11,7 @@ var schemaHomeTemplate = _.template(
 
 
 var jsonObjectHomeTemplate = _.template(
-    '<a href="<%= url %>" class="list-group-item list-group-item-action flex-column align-items-start">' +
+    '<a href="<%= url %>" aria-label="Read more about <%= name %>" class="list-group-item list-group-item-action flex-column align-items-start">' +
         '<div class="d-flex w-100 justify-content-between">' +
             '<h5 class="mb-1"><%= name %></h5>' +
             '<small>updated <%= last_update %></small>' +
