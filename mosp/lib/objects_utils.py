ed709e2a1fb4964d0a50f992f7e416cd7a08e44a
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-06-17 21:18:30 +0200

    fix: [object versioning] fixed display of the revision history table.

--- mosp/lib/objects_utils.py
+++ mosp/lib/objects_utils.py
@@ -65,7 +65,7 @@ def generate_diff(version_before, version_after):
 
     table = difflib.HtmlDiff().make_table(before_json, after_json)
 
-    table = table.replace('<table class="diff"', '<table class="diff"')
+    #table = table.replace('<table class="diff"', '<table class="diff"')
 
     return table
 
