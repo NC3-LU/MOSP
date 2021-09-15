56f25df043a1ba838371d00367207f24742f7d57
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-09-15 09:24:44 +0200

    chg: the API v1 is now in read-only mode.

--- mosp/api/v1/processors.py
+++ mosp/api/v1/processors.py
@@ -6,9 +6,8 @@ from flask import request
 from flask_login import current_user
 from flask_restless import ProcessingException
 
-from mosp.models import User, JsonObject
+from mosp.models import User
 from mosp.views.common import login_user_bundle
-from mosp.api.common import check_submitted_object, create_new_version
 
 logger = logging.getLogger(__name__)
 
@@ -36,39 +35,3 @@ def auth_func(*args, **kw):
         raise ProcessingException("Couldn't authenticate your user", code=401)
 
     login_user_bundle(user)
-
-
-def check_single_object_edit_permission(instance_id, data):
-    """Pre-processor to edit a single object."""
-    if not current_user.is_authenticated:
-        raise ProcessingException(description="Not authenticated!", code=401)
-
-    json_object = JsonObject.query.filter(JsonObject.id == instance_id).first()
-    if json_object:
-        # set the values requires by check_submitted_object()
-        data["schema_id"] = json_object.schema.id
-        data["org_id"] = json_object.organization.id
-        data["creator_id"] = (
-            json_object.creator.id if json_object.creator.id else current_user.id
-        )
-    else:
-        raise ProcessingException(description="Unknown object", code=401)
-    try:
-        check_submitted_object(data)
-    except Exception as e:
-        raise (e)
-
-
-def create_new_version_before_update(instance_id, data):
-    """Use the function `create_new_version` in order to create a new version of the
-    object to update."""
-    create_new_version(instance_id)
-
-
-def check_object_creation_permission(data):
-    """Check if the user is authenticated and set the creator_id."""
-    if not current_user.is_authenticated:
-        raise ProcessingException(description="Not authenticated!", code=401)
-
-    data["creator_id"] = current_user.id
-    check_submitted_object(data)
