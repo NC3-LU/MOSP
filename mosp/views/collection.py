065e77e8e6ee24063ea7a1a432d0f99a012f356c
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-09-21 10:32:18 +0200

    fix: [views] fixed edition of collections

--- mosp/views/collection.py
+++ mosp/views/collection.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, abort
-from flask_login import login_required
+from flask import Blueprint, render_template, abort, flash, redirect, url_for
+from flask_login import login_required, current_user
 from flask_babel import gettext
 
+from mosp.bootstrap import db
 from mosp.models import Collection
 from mosp.forms import CollectionForm
 
@@ -54,5 +55,60 @@ def form(collection_id=None):
         )
 
     # Edition of an existing collection
+    collection = Collection.query.filter(Collection.id == collection_id).first()
+    form = CollectionForm(obj=collection)
+    action = gettext("Edit a collection")
+    head_titles = [action]
+    head_titles.append(collection.name)
 
-    return head_titles
+    return render_template(
+        "edit_collection.html",
+        action=action,
+        head_titles=head_titles,
+        form=form,
+        collection_id=collection_id,
+    )
+
+
+@collection_bp.route("/create", methods=["POST"])
+@collection_bp.route("/edit/<int:collection_id>", methods=["POST"])
+@login_required
+def process_form(collection_id=None):
+    """ "Process the form to edit a collection."""
+    form = CollectionForm()
+
+    if collection_id is not None:
+        collection = Collection.query.filter(Collection.id == collection_id).first()
+
+        form.populate_obj(collection)
+
+        try:
+            db.session.commit()
+            flash(
+                gettext(
+                    "%(object_name)s successfully updated.", object_name=form.name.data
+                ),
+                "success",
+            )
+        except Exception:
+            form.name.errors.append("Name already exists.")
+        return redirect(url_for("collection_bp.form", collection_id=collection.id))
+
+    # Create a new collection
+    new_collection = Collection(
+        name=form.name.data,
+        description=form.description.data,
+        creator_id=current_user.id,
+    )
+    db.session.add(new_collection)
+    try:
+        db.session.commit()
+        flash(
+            gettext(
+                "%(object_name)s successfully created.", object_name=new_collection.name
+            ),
+            "success",
+        )
+    except Exception:
+        return redirect(url_for("collection_bp.form"))
+    return redirect(url_for("collection_bp.form", collection_id=new_collection.id))
