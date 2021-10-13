ae49f2f9ed291d00f64cea0ce4149609f197fce8
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-10-13 15:09:40 +0200

    chg: [internal] add de language code

--- mosp/bootstrap.py
+++ mosp/bootstrap.py
@@ -89,7 +89,7 @@ def get_locale():
     # otherwise try to guess the language from the user accept
     # header the browser transmits.  We support de/fr/en in this
     # example.  The best match wins.
-    return request.accept_languages.best_match(["fr", "en"])
+    return request.accept_languages.best_match(["fr", "en", "de"])
 
 
 # @babel.timezoneselector
