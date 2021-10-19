ef8a79240d2b4e4aa0d881754e8b38f51b2fee0c
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-10-19 15:59:02 +0200

    @minor do not open fr/LC_MESSAGES/messages.po

--- mosp/translations/internationalization.sh
+++ mosp/translations/internationalization.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 pybabel extract -F mosp/translations/babel.cfg -k lazy_gettext -o mosp/translations/messages.pot mosp/
-poedit mosp/translations/fr/LC_MESSAGES/messages.po
+#poedit mosp/translations/fr/LC_MESSAGES/messages.po
