d37fbafeaae8d89daae4d6dd839a6ec27f30cb41
Cédric Bonhomme <cedric@cedricbonhomme.org>
2022-06-13 14:24:52 +0200

    updated entrypoint.sh !minor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 
 if [ "$DEBUG" ]; then
-	set -x
+    set -x
 fi
 set -eu
 
 FLASK_ENV="$ENVIRONMENT"
 FLASK_DEBUG="$DEBUG"
+FLASK_APP="$FLASK_APP"
 FLASK_RUN_HOST="$HOST"
 FLASK_RUN_PORT="$PORT"
 
-export FLASK_ENV FLASK_DEBUG FLASK_RUN_HOST FLASK_RUN_PORT
+export FLASK_ENV FLASK_DEBUG FLASK_APP FLASK_RUN_HOST FLASK_RUN_PORT
 
 prepare_db() {
-	flask db_create || true
-	flask db_init
-	flask db upgrade
-	flask import_licenses_from_spdx
+    flask db_create || true
+    flask db_init
+    flask db upgrade
+    flask import_licenses_from_spdx
     flask create_admin --login admin --email admin@admin.localhost. --password password || true
 }
 
 # waiting for DB to come up
 for try in 1 2 3 4 5 6; do
-	echo >&2 "migration - attempt $try"
-	prepare_db && break || true
-	sleep 5
-	[ "$try" = "6" ] && exit 1
+    echo >&2 "migration - attempt $try"
+    prepare_db && break || true
+    sleep 5
+    [ "$try" = "6" ] && exit 1
 done
 
 gunicorn --chdir /app --workers 2 --bind 0.0.0.0:5000 runserver:application
