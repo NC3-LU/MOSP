f0462d3c39c9a4547515d7ac3787f3114ce3f633
Cédric Bonhomme <cedric@cedricbonhomme.org>
2022-06-13 14:17:16 +0200

    updatet get version

--- mosp/__init__.py
+++ mosp/__init__.py
@@ -1,13 +1,29 @@
 import os
 import subprocess
 
+import pkg_resources
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-__version__ = (
-    os.environ.get("PKGVER")
-    or subprocess.run(
-        ["git", "-C", BASE_DIR, "describe", "--tags"], stdout=subprocess.PIPE
-    )
-    .stdout.decode()
-    .strip()
-)
+
+def get_version():
+    if os.getenv("MOSP_VERSION", False):
+        return os.getenv("MOSP_VERSION", "")
+
+    version = (
+        os.environ.get("PKGVER")
+        or subprocess.run(
+            ["git", "-C", BASE_DIR, "describe", "--tags"], stdout=subprocess.PIPE
+        )
+        .stdout.decode()
+        .strip()
+    ) or ""
+    if not version:
+        try:
+            version = pkg_resources.get_distribution("mosp").version
+        except pkg_resources.DistributionNotFound:
+            version = ""
+    return version
+
+
+__version__ = get_version()
